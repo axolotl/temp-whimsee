e10639c280446baa3606e988f45f501abebf5c6b
Axolotl <samuelmachat@gmail.com>
2018-11-20 20:54:14 -0500

    create splash page with image and text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import image from './img/whimsee-cover.png'
+
+const Container = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Splash = styled.img`
+  max-width: 600px;
+`
+
+const Text = styled.h1`
+  font-weight: normal; 
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container>
+        <Splash src={image} alt="cover" />
+        <Text>Website coming soon!</Text>
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
